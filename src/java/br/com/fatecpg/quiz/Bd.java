044459fa0012114e75d8470985c6ffe4b077902e
Guigso <gui.soliveira@hotmail.com>
2018-11-09 23:51:16 -0200

    esqueci de dar git add

--- src/java/br/com/fatecpg/quiz/Bd.java
+++ src/java/br/com/fatecpg/quiz/Bd.java
@@ -8,16 +8,16 @@ package br.com.fatecpg.quiz;
 import java.util.ArrayList;
 import java.util.Collections;
 
+
 /**
  *
  * @author Positivo
  */
 public class Bd {
-
     private static ArrayList<Usuario> users;
 
-    public static ArrayList<Usuario> getUsuario() {
-        if (users == null) {
+    public static ArrayList<Usuario> getUsuario(){
+        if(users == null){
             users = new ArrayList<>();
             Usuario user = new Usuario("EduardoSimao", "1234");
             users.add(user);
@@ -37,6 +37,11 @@ public class Bd {
         test.add(new Question("8Qual desses é um mámifero?", "Ornitorrinco", new String[]{"Tubarão Branco", "Ornitorrinco", "Pato"}));
         test.add(new Question("9Qual desses é um mámifero?", "Ornitorrinco", new String[]{"Tubarão Branco", "Ornitorrinco", "Pato"}));
         test.add(new Question("0Qual desses é um mámifero?", "Ornitorrinco", new String[]{"Tubarão Branco", "Ornitorrinco", "Pato"}));
+        test.add(new Question("11Qual desses é um mámifero?", "Ornitorrinco", new String[]{"Tubarão Branco", "Ornitorrinco", "Pato"}));
+        test.add(new Question("12Qual desses é um mámifero?", "Ornitorrinco", new String[]{"Tubarão Branco", "Ornitorrinco", "Pato"}));
+        test.add(new Question("13Qual desses é um mámifero?", "Ornitorrinco", new String[]{"Tubarão Branco", "Ornitorrinco", "Pato"}));
+        test.add(new Question("14Qual desses é um mámifero?", "Ornitorrinco", new String[]{"Tubarão Branco", "Ornitorrinco", "Pato"}));
+        test.add(new Question("15Qual desses é um mámifero?", "Ornitorrinco", new String[]{"Tubarão Branco", "Ornitorrinco", "Pato"}));
         Collections.shuffle(test);
         return test;
     }
